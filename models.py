41bf3fc879d37042a873312d76dd4b5582ef5042
Kyle Marshall <kylemarshall88@gmail.com>
2017-07-11 23:47:56 -0700

    fc layers; decoder skeleton

--- models.py
+++ models.py
@@ -2,14 +2,36 @@ import os
 import sys
 import numpy as np
 import tensorflow as tf
-from tensorflow.contrib.rnn import LSTMCell
+from tensorflow.contrib.rnn import (LSTMCell,
+									GRUCell,
+									MultiRNNCell)
+import tensorflow.contrib.seq2seq as seq2seq
 
 class EncoderModel(object):
 	"""Core training model"""
-	def __init__(self, max_len, hidden_units, lr):
+	def __init__(self, input_shape, max_seq_len, vocab_size, cell_size, num_layers, lr=5e-5, cell_type='gru'):
 		# Placeholders
-		self.input_node = tf.placeholder(tf.float32, [None, max_len, 5])
-		self.labels = tf.placeholder(tf.float32, [None])
+		self.input_node = tf.placeholder(tf.float32, [None, input_shape])
+		self.label_node = tf.placeholder(tf.int8, [None, max_seq_len, vocab_size])
+
+		w1 = tf.Variable(tf.random_normal([input_shape, 128]))
+		b1 = tf.Variable(tf.random_normal([128]))
+		w2 = tf.Variable(tf.random_normal([128, 64]))
+		b2 = tf.Variable(tf.random_normal([64]))
+
+		layer_1 = tf.nn.relu(tf.add(tf.matmul(self.input_node, w1), b1))
+		layer_2 = tf.nn.relu(tf.add(tf.matmul(layer_1, w2), b2))
+
+		if cell_type == 'gru':
+			cell_class = GRUCell
+		elif cell_type == 'lstm':
+			cell_class = LSTMCell
+		else:
+			raise ValueError("Cell type '%s' not valid"%cell_type)
+
+		self.cell = single_cell = cell_class(cell_size)
+		if num_layers > 1:
+			self.cell = MultiRNNCell([single_cell] * num_layers)
 
 		# weights = tf.Variable(tf.random_normal([hidden_units, 1]))
 		# biases = tf.Variable(tf.random_normal([1]))
--- train.py
+++ train.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 
 from utils import DataSet
-from models import EncoderModel
+from models import EncoderModel as Model
 
 tf.app.flags.DEFINE_string("dataset", None, 'Path to dataset npz.')
 tf.app.flags.DEFINE_integer("batch_size", 128, 'Size of train and valid batches.')
@@ -12,10 +12,16 @@ def main(*args):
 	learning_rate = 0.001
 	training_steps = 10000
 	valid_step = 50
+	cell_size = 256
+	num_rnn_layers = 2
 
 	dataset = DataSet(FLAGS.dataset)
-	print(dataset.samples_shape)
-	print(dataset.labels_shape)
+	model = Model(dataset.samples_shape[1],
+				  dataset.labels_shape[1],
+				  dataset.labels_shape[2],
+				  cell_size,
+				  num_rnn_layers,
+				  learning_rate)
 
 if __name__ == "__main__":
     tf.app.run()
